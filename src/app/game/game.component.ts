469442808598d688f659d0b177596c3cee6982e2
Leon Groschek <leon.groschek12@gmail.com>
2022-12-18 13:14:43 +0100

    fail

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -24,20 +24,20 @@ export class GameComponent implements OnInit{
     this.gameId = params.id;
 
     this
-      .firestore
-      .collection('Game')
-      .doc(this.gameId)
-      .valueChanges()
-      .subscribe((game: any) => {
-        this.game.players = game.players,
-        this.game.stack = game.stack,
-        this.game.playedCards = game.playedCards,
-        this.game.currentPlayer = game.currentPlayer,
-        this.game.pickCardAnimation = game.pickCardAnimation,
-        this.game.currentCard = game.currentCard
+    .firestore
+    .collection('Game')
+    .doc(this.gameId)
+    .valueChanges()
+    .subscribe(async (game: any) => {
+      this.game.players = game.players,
+      this.game.stack = game.stack,
+      this.game.playedCards = game.playedCards,
+      this.game.currentPlayer = game.currentPlayer,
+      this.game.pickCardAnimation = game.pickCardAnimation,
+      this.game.currentCard = game.currentCard
+      console.log('Game', this.game)
       })
     })
-    debugger
   }
   
   newGame() {
@@ -48,11 +48,11 @@ export class GameComponent implements OnInit{
     console.log("Game update", this.game.players);  
     if(!this.game.pickCardAnimation) {
       this.game.currentCard = this.game.stack.pop();
-      this.saveGame();
       this.game.pickCardAnimation = true;
-
+      
       this.game.currentPlayer++;
       this.game.currentPlayer = this.game.currentPlayer % this.game.players.length;
+      this.saveGame();
       
       setTimeout(() => {
         this.game.playedCards.push(this.game.currentCard);
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyCoXfKQBs8wIS1OX2SWjZY3wrNgf1va-IM',
+        authDomain: 'project-ring-of-fire.firebaseapp.com',
+        projectId: 'project-ring-of-fire',
+        storageBucket: 'project-ring-of-fire.appspot.com',
+        messagingSenderId: '647414647227',
+        appId: '1:647414647227:web:efe4dd957f6870a107dc9e'
+      }
+}
